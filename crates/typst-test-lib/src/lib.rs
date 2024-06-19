93fc339f5df6beb8c38b9214d055dec2056490d9
tinger <me@tinger.dev>
2024-06-19 19:51:57 +0200

    Added common tests on-disk operations

--- crates/typst-test-lib/src/lib.rs
+++ crates/typst-test-lib/src/lib.rs
@@ -10,3 +10,69 @@ pub mod util;
 
 #[cfg(test)]
 pub mod _dev;
+
+#[cfg(test)]
+#[cfg(test)]
+mod tests {
+    use store::project::legacy::ProjectLegacy;
+    use test::collector::Collector;
+    use typst::eval::Tracer;
+
+    use super::*;
+    use crate::_dev::GlobalTestWorld;
+    use crate::compile::Metrics;
+    use crate::store::page::Png;
+    use crate::{compare, compile, library, render};
+
+    #[test]
+    fn test_e2e() {
+        let world = GlobalTestWorld::new(library::augmented_default_library());
+        let project = ProjectLegacy::new("../../", "assets/test-assets/collect");
+
+        let strategy = render::Strategy::default();
+
+        let mut collector = Collector::new(&project);
+        collector.collect();
+
+        for test in collector.tests().values() {
+            let source = test.load_test_source(&project).unwrap();
+            let output = compile::compile(
+                source.clone(),
+                &world,
+                &mut Tracer::new(),
+                &mut Metrics::new(),
+            )
+            .unwrap();
+
+            if test.is_compile_only() {
+                continue;
+            }
+
+            let output = render::render_document(&output, strategy);
+
+            let reference = if let Some(reference) = test.load_ref_source(&project).unwrap() {
+                let reference = compile::compile(
+                    reference.clone(),
+                    &world,
+                    &mut Tracer::new(),
+                    &mut Metrics::new(),
+                )
+                .unwrap();
+
+                render::render_document(&reference, strategy).collect()
+            } else if let Some(pages) = test.load_ref_pages::<Png, _>(&project).unwrap() {
+                pages
+            } else {
+                panic!()
+            };
+
+            compare::visual::compare_pages(
+                output,
+                reference.into_iter(),
+                compare::visual::Strategy::default(),
+                false,
+            )
+            .unwrap();
+        }
+    }
+}
--- crates/typst-test-lib/src/test.rs
+++ crates/typst-test-lib/src/test.rs
@@ -1,6 +1,8 @@
 use std::fmt::Debug;
-use std::io;
+use std::fs::File;
+use std::io::{self, Write};
 
+use ecow::EcoString;
 use id::Identifier;
 use typst::syntax::{FileId, Source, VirtualPath};
 
@@ -12,11 +14,23 @@ pub mod collector;
 pub mod id;
 pub mod matcher;
 
+/// A thin test handle for managing on-disk resources.
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Test {
     id: Identifier,
     ref_kind: Option<ReferenceKind>,
     is_ignored: bool,
+}
+
+/// References for a test.
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub enum References<F: PageFormat> {
+    /// An ephemeral reference script used to compile the reference document on
+    /// the fly.
+    Ephemeral(EcoString),
+
+    /// Persistent references which are stored on disk.
+    Persistent(Vec<F::Type>),
 }
 
 impl Test {
@@ -59,8 +73,98 @@ impl Test {
         self.is_ignored
     }
 
+    /// Creates a new test directly on disk.
+    pub fn create<F: PageFormat, P: Project>(
+        id: Identifier,
+        project: &P,
+        source: &str,
+        references: Option<References<F>>,
+    ) -> Result<Self, SaveError<F>> {
+        let test_dir = project.resolve(&id, TestTarget::TestDir);
+        util::fs::create_dir(test_dir, true)?;
+
+        let mut file = File::options()
+            .write(true)
+            .create_new(true)
+            .open(project.resolve(&id, TestTarget::TestScript))?;
+
+        file.write_all(source.as_bytes())?;
+
+        let ref_kind = match references {
+            Some(References::Ephemeral(_)) => Some(ReferenceKind::Ephemeral),
+            Some(References::Persistent(_)) => Some(ReferenceKind::Persistent),
+            None => None,
+        };
+
+        let is_ignored = source
+            .lines()
+            .take_while(|&l| l.starts_with("///"))
+            .filter(|l| {
+                l.strip_prefix("///")
+                    .is_some_and(|l| l.trim() == "[ignored]")
+            })
+            .next()
+            .is_some();
+
+        let test = Self {
+            id,
+            ref_kind,
+            is_ignored,
+        };
+
+        match references {
+            Some(References::Ephemeral(reference)) => {
+                test.create_reference_script(project, reference.as_str())?;
+            }
+            Some(References::Persistent(pages)) => {
+                test.create_reference_pages(project, pages.iter())?;
+            }
+            None => {}
+        }
+
+        Ok(test)
+    }
+
+    /// Creates this test's temporary directories.
+    pub fn create_temporary_directories<P: Project>(&self, project: &P) -> io::Result<()> {
+        if self.is_ephemeral() {
+            util::fs::create_dir(project.resolve(&self.id, TestTarget::RefDir), true)?;
+        }
+
+        util::fs::create_dir(project.resolve(&self.id, TestTarget::OutDir), true)?;
+        util::fs::create_dir(project.resolve(&self.id, TestTarget::DiffDir), true)?;
+        Ok(())
+    }
+
+    /// Creates this test's reference script.
+    pub fn create_reference_script<P: Project>(
+        &self,
+        project: &P,
+        reference: &str,
+    ) -> io::Result<()> {
+        std::fs::write(project.resolve(&self.id, TestTarget::RefScript), reference)?;
+        Ok(())
+    }
+
+    /// Creates this test's persistent references.
+    pub fn create_reference_pages<'p, F: PageFormat, P: Project>(
+        &self,
+        project: &P,
+        pages: impl IntoIterator<Item = &'p F::Type>,
+    ) -> Result<(), SaveError<F>>
+    where
+        F::Type: 'p,
+    {
+        // TODO: the error handling is slightly inconvenient here, we don't
+        // want this to be a format dependent error
+        let ref_dir = project.resolve(&self.id, TestTarget::RefDir);
+        util::fs::create_dir(ref_dir, true)?;
+        store::page::save_pages::<F>(ref_dir, pages)?;
+        Ok(())
+    }
+
     /// Deletes this test's directories and scripts.
-    pub fn delete_test<P: Project>(self, project: &P) -> io::Result<()> {
+    pub fn delete<P: Project>(self, project: &P) -> io::Result<()> {
         self.delete_reference_pages(project)?;
         self.delete_reference_script(project)?;
         self.delete_temporary_directories(project)?;
@@ -97,8 +201,8 @@ impl Test {
     /// Removes any previous references and creates a reference script by
     /// copying the test script.
     pub fn make_ephemeral<P: Project>(&mut self, project: &P) -> io::Result<()> {
-        self.delete_reference_pages(project)?;
         self.delete_reference_script(project)?;
+        self.delete_reference_pages(project)?;
 
         std::fs::copy(
             project.resolve(&self.id, TestTarget::TestScript),
@@ -119,17 +223,16 @@ impl Test {
     where
         F::Type: 'p,
     {
-        // TODO: the error handling is slightly wrong here
-        // the upper io error should not be converted into a SaveError<F>::Io
+        self.delete_reference_script(project)?;
         self.delete_reference_pages(project)?;
-        store::page::save_pages::<F>(project.resolve(&self.id, TestTarget::RefDir), pages)?;
+        self.create_reference_pages(project, pages)?;
 
         self.ref_kind = Some(ReferenceKind::Persistent);
         Ok(())
     }
 
     /// Removes any previous references.
-    pub fn make_compile_only<F: PageFormat, P: Project>(&mut self, project: &P) -> io::Result<()> {
+    pub fn make_compile_only<P: Project>(&mut self, project: &P) -> io::Result<()> {
         self.delete_reference_pages(project)?;
         self.delete_reference_script(project)?;
 
@@ -186,79 +289,168 @@ pub enum ReferenceKind {
 
 #[cfg(test)]
 mod tests {
-    use collector::Collector;
+    use store::page::Png;
     use store::project::legacy::ProjectLegacy;
-    use typst::eval::Tracer;
 
     use super::*;
-    use crate::_dev::GlobalTestWorld;
-    use crate::compile::Metrics;
-    use crate::store::page::Png;
-    use crate::{compare, compile, library, render};
+    use crate::_dev;
+    use crate::_dev::fs::Setup;
+
+    fn setup_all(root: &mut Setup) -> &mut Setup {
+        root.setup_file("tests/compile-only/test.typ", "Hello World")
+            .setup_file("tests/ephemeral/test.typ", "Hello World")
+            .setup_file("tests/ephemeral/ref.typ", "Hello\nWorld")
+            .setup_file("tests/persistent/test.typ", "Hello World")
+            .setup_dir("tests/persistent/ref")
+    }
 
     #[test]
-    fn test_e2e() {
-        let world = GlobalTestWorld::new(library::augmented_default_library());
-        let project = ProjectLegacy::new("../../", "assets/test-assets/collect");
-
-        let strategy = render::Strategy::default();
-
-        let mut collector = Collector::new(&project);
-        collector.collect();
-
-        for test in collector.tests().values() {
-            let source = test.load_test_source(&project).unwrap();
-            let output = compile::compile(
-                source.clone(),
-                &world,
-                &mut Tracer::new(),
-                &mut Metrics::new(),
-            )
-            .unwrap();
-
-            if test.is_compile_only() {
-                continue;
-            }
-
-            let output = render::render_document(&output, strategy);
-
-            let reference = if let Some(reference) = test.load_ref_source(&project).unwrap() {
-                let reference = compile::compile(
-                    reference.clone(),
-                    &world,
-                    &mut Tracer::new(),
-                    &mut Metrics::new(),
+    fn test_create_new() {
+        _dev::fs::TempEnv::run(
+            |root| root.setup_dir("tests"),
+            |root| {
+                let project = ProjectLegacy::new(root, "tests");
+                Test::create::<Png, _>(
+                    Identifier::new("compile-only").unwrap(),
+                    &project,
+                    "Hello World",
+                    None,
                 )
                 .unwrap();
 
-                render::render_document(&reference, strategy).collect()
-            } else if let Some(pages) = test.load_ref_pages::<Png, _>(&project).unwrap() {
-                pages
-            } else {
-                panic!()
-            };
+                Test::create::<Png, _>(
+                    Identifier::new("ephemeral").unwrap(),
+                    &project,
+                    "Hello World",
+                    Some(References::Ephemeral("Hello\nWorld".into())),
+                )
+                .unwrap();
 
-            compare::visual::compare_pages(
-                output,
-                reference.into_iter(),
-                compare::visual::Strategy::default(),
-                false,
-            )
-            .unwrap();
-        }
+                Test::create::<Png, _>(
+                    Identifier::new("persistent").unwrap(),
+                    &project,
+                    "Hello World",
+                    Some(References::Persistent(vec![])),
+                )
+                .unwrap();
+            },
+            |root| {
+                root.expect_file("tests/compile-only/test.typ", "Hello World")
+                    .expect_file("tests/ephemeral/test.typ", "Hello World")
+                    .expect_file("tests/ephemeral/ref.typ", "Hello\nWorld")
+                    .expect_file("tests/persistent/test.typ", "Hello World")
+                    .expect_dir("tests/persistent/ref")
+            },
+        );
+    }
+
+    #[test]
+    fn test_make_ephemeral() {
+        _dev::fs::TempEnv::run(
+            setup_all,
+            |root| {
+                let project = ProjectLegacy::new(root, "tests");
+                Test::new(Identifier::new("compile-only").unwrap())
+                    .make_ephemeral(&project)
+                    .unwrap();
+
+                Test::new(Identifier::new("ephemeral").unwrap())
+                    .make_ephemeral(&project)
+                    .unwrap();
+
+                Test::new(Identifier::new("persistent").unwrap())
+                    .make_ephemeral(&project)
+                    .unwrap();
+            },
+            |root| {
+                root.expect_file("tests/compile-only/test.typ", "Hello World")
+                    .expect_file("tests/compile-only/ref.typ", "Hello World")
+                    .expect_file("tests/ephemeral/test.typ", "Hello World")
+                    .expect_file("tests/ephemeral/ref.typ", "Hello World")
+                    .expect_file("tests/persistent/test.typ", "Hello World")
+                    .expect_file("tests/persistent/ref.typ", "Hello World")
+            },
+        );
+    }
+
+    #[test]
+    fn test_make_persistent() {
+        _dev::fs::TempEnv::run(
+            setup_all,
+            |root| {
+                let project = ProjectLegacy::new(root, "tests");
+                Test::new(Identifier::new("compile-only").unwrap())
+                    .make_persistent::<Png, _>(&project, [])
+                    .unwrap();
+
+                Test::new(Identifier::new("ephemeral").unwrap())
+                    .make_persistent::<Png, _>(&project, [])
+                    .unwrap();
+
+                Test::new(Identifier::new("persistent").unwrap())
+                    .make_persistent::<Png, _>(&project, [])
+                    .unwrap();
+            },
+            |root| {
+                root.expect_file("tests/compile-only/test.typ", "Hello World")
+                    .expect_dir("tests/compile-only/ref")
+                    .expect_file("tests/ephemeral/test.typ", "Hello World")
+                    .expect_dir("tests/ephemeral/ref")
+                    .expect_file("tests/persistent/test.typ", "Hello World")
+                    .expect_dir("tests/persistent/ref")
+            },
+        );
+    }
+
+    #[test]
+    fn test_make_compile_only() {
+        _dev::fs::TempEnv::run(
+            setup_all,
+            |root| {
+                let project = ProjectLegacy::new(root, "tests");
+                Test::new(Identifier::new("compile-only").unwrap())
+                    .make_compile_only(&project)
+                    .unwrap();
+
+                Test::new(Identifier::new("ephemeral").unwrap())
+                    .make_compile_only(&project)
+                    .unwrap();
+
+                Test::new(Identifier::new("persistent").unwrap())
+                    .make_compile_only(&project)
+                    .unwrap();
+            },
+            |root| {
+                root.expect_file("tests/compile-only/test.typ", "Hello World")
+                    .expect_file("tests/ephemeral/test.typ", "Hello World")
+                    .expect_file("tests/persistent/test.typ", "Hello World")
+            },
+        );
     }
 
     #[test]
     fn test_load_sources() {
-        let project = ProjectLegacy::new("../../", "assets/test-assets");
+        _dev::fs::TempEnv::run(
+            |root| {
+                root.setup_file("tests/fancy/test.typ", "Hello World")
+                    .setup_file("tests/fancy/ref.typ", "Hello\nWorld")
+            },
+            |root| {
+                let project = ProjectLegacy::new(root, "tests");
 
-        let test = Test {
-            id: Identifier::new("collect/compare/ephemeral-no-store").unwrap(),
-            ref_kind: Some(ReferenceKind::Ephemeral),
-            is_ignored: false,
-        };
+                let test = Test {
+                    id: Identifier::new("fancy").unwrap(),
+                    ref_kind: Some(ReferenceKind::Ephemeral),
+                    is_ignored: false,
+                };
 
-        test.load_test_source(&project).unwrap();
-        test.load_ref_source(&project).unwrap().unwrap();
+                test.load_test_source(&project).unwrap();
+                test.load_ref_source(&project).unwrap().unwrap();
+            },
+            |root| {
+                root.expect_file("tests/fancy/test.typ", "Hello World")
+                    .expect_file("tests/fancy/ref.typ", "Hello\nWorld")
+            },
+        );
     }
 }
